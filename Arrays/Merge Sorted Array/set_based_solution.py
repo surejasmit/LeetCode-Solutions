79706798b9c606c9cfd33b1d6b897478a60d0d51
surejasmit <smitsureja472007@gmail.com>
2026-01-27 23:54:57 +0530

    Update set_based_solution.py

--- Arrays/Merge Sorted Array/set_based_solution.py
+++ Arrays/Merge Sorted Array/set_based_solution.py
@@ -1,18 +1,1 @@
-def remove_duplicates(nums):
-    seen = set()
-    index = 0
 
-    for num in nums:
-        if num not in seen:
-            seen.add(num)
-            nums[index] = num
-            index += 1
-
-    return index
-
-
-# Example usage
-nums = [0,0,1,1,1,2,2,3,3,4]
-k = remove_duplicates(nums)
-print(k)        # Output: 5
-print(nums)     # [0,1,2,3,4,2,2,3,3,4]
